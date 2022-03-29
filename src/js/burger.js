a57e5e0cd92a111e46fdab66676061fadd13fa0d
JuliaVasiukevich <julliavas@yandex.ru>
2022-03-30 00:52:26 +0300

    Add header feachures

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".header__burger").click(function (event) {
-    $(".header__burger,.header__menu-list").toggleClass("active");
+    $(".header__burger,.header__menu-list, .hero__content").toggleClass("active");
     $("body").toggleClass("lock");
   });
 });
